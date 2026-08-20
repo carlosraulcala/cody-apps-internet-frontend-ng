66fec59ad82b89c0cac2b1ee7dfcd5feb3fa4643
carlos cala <calacamatacarlosraul@gmail.com>
2026-08-19 20:02:57 -0400

    agregando las rutas del frontend

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,15 +8,16 @@ import { SignInComponent } from './features/auth/pages/sign-in/sign-in.component
 import { SignUpComponent } from './features/auth/pages/sign-up/sign-up.component';
 
 import { CategoryListComponent } from './features/categories/components/category-list/category-list.component';
-import { ProductsComponent } from './features/products/components/product-list/products.component';
-import { CartComponent } from './features/cart/components/cart-view/cart.component';
+import { ProductListComponent } from '@features/products/components/product-list/product-list.component';
+import { CartComponent } from '@features/cart/components/cart-view/cart.component';
+import { ReviewListComponent } from '@features/reviews/components/review-list/review-list.component';
 
 export const routes: Routes = [
   {
-    path:'',
-    component:AppLayoutComponent,
+    path: '',
+    component: AppLayoutComponent,
     canActivate: [authGuard],
-    children:[
+    children: [
       {
         path: '',
         component: DashboardComponent,
@@ -24,42 +25,47 @@ export const routes: Routes = [
         title: 'MiPanel Pro | Dashboard',
       },
       {
-        path:'tasks',
-        component:TasksComponent,
-        title:'Gestión de Tareas | MiPanel Pro'
+        path: 'tasks',
+        component: TasksComponent,
+        title: 'Gestión de Tareas | MiPanel Pro'
       },
       {
-        path:'categories',
-        component:CategoryListComponent,
-        title:'Categorías | MiPanel Pro'
+        path: 'categories',
+        component: CategoryListComponent,
+        title: 'Categorías | MiPanel Pro'
       },
       {
-        path:'products',
-        component:ProductsComponent,
-        title:'Productos | MiPanel Pro'
+        path: 'products',
+        component: ProductListComponent,
+        title: 'Productos | MiPanel Pro'
       },
       {
-        path:'cart',
-        component:CartComponent,
-        title:'Carrito | MiPanel Pro'
+        path: 'cart',
+        component: CartComponent,
+        title: 'Carrito | MiPanel Pro'
+      },
+      {
+        path: 'reviews',
+        component: ReviewListComponent,
+        title: 'Reseñas | MiPanel Pro'
       }
     ]
   },
   // auth pages
   {
-    path:'signin',
-    component:SignInComponent,
-    title:'Iniciar Sesión | MiPanel Pro'
+    path: 'signin',
+    component: SignInComponent,
+    title: 'Iniciar Sesión | MiPanel Pro'
   },
   {
-    path:'signup',
-    component:SignUpComponent,
-    title:'Registro | MiPanel Pro'
+    path: 'signup',
+    component: SignUpComponent,
+    title: 'Registro | MiPanel Pro'
   },
   // error pages
   {
-    path:'**',
-    component:NotFoundComponent,
-    title:'Página no encontrada | MiPanel Pro'
+    path: '**',
+    component: NotFoundComponent,
+    title: 'Página no encontrada | MiPanel Pro'
   },
 ];
